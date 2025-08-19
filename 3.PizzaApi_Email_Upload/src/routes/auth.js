b4e92d378650b69e65cfd5349fcfb39b8bbbbd50
MehmetTurkerTuncer-UIA <mehmettt@student.uia.no>
2025-08-19 20:39:52 +0200

    PizzaApi_Email_Upload

---
+++ 3.PizzaApi_Email_Upload/src/routes/auth.js
@@ -0,0 +1,22 @@
+"use strict";
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+const router = require("express").Router();
+/* ------------------------------------------------------- */
+// routes/auth:
+
+const auth = require("../controllers/auth");
+
+// URL: /auth
+
+// Login/logout:
+router.post("/login", auth.login);
+// refresh
+router.post("/refresh", auth.refresh);  // Refresh JWT
+// router.all('/logout', auth.logout)
+router.get("/logout", auth.logout);
+
+/* ------------------------------------------------------- */
+module.exports = router;
+
