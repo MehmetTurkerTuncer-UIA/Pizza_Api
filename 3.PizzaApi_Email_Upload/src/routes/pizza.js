e25a05cb42aeb3fa3ab5cfb16a42441d5b3aad5f
MehmetTurkerTuncer-UIA <mehmettt@student.uia.no>
2025-08-19 23:38:41 +0200

    uploadFile

--- 3.PizzaApi_Email_Upload/src/routes/pizza.js
+++ 3.PizzaApi_Email_Upload/src/routes/pizza.js
@@ -4,13 +4,32 @@
 ------------------------------------------------------- */
 const router = require("express").Router();
 /* ------------------------------------------------------- */
+
+// UPLOAD
+// npm i multer
+// https://expressjs.com/en/resources/middleware/multer.html
+
+const multer = require("multer");
+const upload = multer({ 
+    //dest: "./uploads/",  
+    storage: multer.diskStorage({
+        destination: './uploads/', 
+    })
+  }); // Files will be saved in the '
+
+
+/* ------------------------------------------------------- */
 // routes/pizza:
 
 const pizza = require("../controllers/pizza");
 
 // URL: /pizzas
 
-router.route("/").get(pizza.list).post(pizza.create);
+router.route("/")
+  .get(pizza.list)
+  //.post(upload.single(image), pizza.create);
+  //.post(upload.array(image), pizza.create);
+  .post(upload.any(), pizza.create);
 
 router
   .route("/:id")
